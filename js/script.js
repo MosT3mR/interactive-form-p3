e6bffa14045a5035aa2fd9d92f0f4063cf96024d
MosT3mR <most3mr@live.com>
2021-08-03 01:08:28 +0300

    fixing bugs

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
-document.getElementById('name').focus(); //The "Name" field fouces, we can add addEventListener("DOMContentLoaded")
+/*
+Treehouse Techdegree:
+FSJS Project 2 - Data Pagination and Filtering
+Student : Othman Alomair
+*/
 
-
-/* 
+/*
 Job Role
 */
 
-// first we hide 'the other job role if the select menu is other we show it
-
+const customerName = document.getElementById('name');
 const otherJobRole = document.getElementById('other-job-role');
-otherJobRole.style.display = 'none';
-const JobRole = document.getElementById('title'); // we add the eventlistenr to the job role only
+const JobRole = document.getElementById('title'); 
 
-JobRole.addEventListener('change', (e) => {
+customerName.focus(); //The "Name" field fouces
+
+otherJobRole.style.display = 'none'; //"Other job role" text field displays/hides when a user selects/deselects "Other" from the Job Role menu
+JobRole.addEventListener('change', (e) => { 
     if(e.target.value === 'other'){
         otherJobRole.style.display = '';
     } else {
@@ -19,33 +23,26 @@ JobRole.addEventListener('change', (e) => {
     }
 });
 
-
 /*
 T-Shirts
 */
 
-const shirtList = document.getElementById('color');
-const shirtColors = document.getElementById('shirt-colors');
-shirtColors.style.display = 'none';
+const design = document.getElementById('design');
+const color = document.getElementById('color');
+const shirtList = color.children;
+color.disabled = true;
 
-const shirtDesigns = document.getElementById('shirt-designs');
-
-const showAvailable = function(option){
-    shirtColors.style.display = '';
+design.addEventListener('change', (e) => {
+    color.disabled = false;
     for(let i = 0; i < shirtList.length; i++){
-        shirtList[i].setAttribute('hidden','');
-        if(shirtList[i].getAttribute('data-theme') === option){
-            shirtList[i].removeAttribute('hidden');
+        if(shirtList[i].getAttribute('data-theme') === design.value){
+            shirtList[i].selected = true;
+            shirtList[i].hidden = false;
+        } else {
+            shirtList[i].selected = false;
+            shirtList[i].hidden = true;
         }
     }
-}
-
-shirtDesigns.addEventListener('change', (e) => {
-    if(e.target.value === 'js puns') {
-        showAvailable('js puns');
-    } else if (e.target.value === 'heart js') {
-        showAvailable('heart js');
-    } 
 });
 
 
@@ -100,35 +97,80 @@ payment.addEventListener('change', (e) => {
 Validation
 */
 
-// Must be a valid email address
-function isValidEmail(email) {
-    return /^[^@]+@[^@.]+\.[a-z]+$/i.test(email);
-}
-
-// The "Card number" field must contain a 13 - 16 digit credit card number with no dashes or spaces.
-function isValidccNum(ccNum) {
-    return /^\d{13}$/.test(ccNum) | /^\d{16}$/.test(ccNum);
-}
-
-// The "Zip code" field must contain a 5 digit number.
-function isValidZip(zip) {
-    return /^\d{5}$/.test(zip);
-}
-
-// The "CVV" field must contain a 3 digit number.
-function isValidCvv(cvv) {
-    return /^\d{3}$/.test(cvv);
-}
-
+const form = document.querySelector('form');
 const email = document.getElementById('email');
 const ccNum = document.getElementById('cc-num');
 const zip = document.getElementById('zip');
 const cvv = document.getElementById('cvv');
 
 
+// testing
 
+function isValidName(name) {
+    return /^[A-Za-z]+$/.test(name);
+}
+
+function isValidEmail(email) {
+    return /^[^@]+@[^@.]+\.[a-z]+$/i.test(email);
+}
+
+function isValidccNum(ccNum) {
+    return /^\d{13}$/.test(ccNum) | /^\d{16}$/.test(ccNum);
+}
+
+function isValidZip(zip) {
+    return /^\d{5}$/.test(zip);
+}
+
+function isValidCvv(cvv) {
+    return /^\d{3}$/.test(cvv);
+}
+
+function isValidRegister(total) {
+    if(total > 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isValidPayment() {
+    if (payment.value === 'credit-card'){
+        if(isValidccNum(ccNum.value) && isValidZip(zip.value) && isValidCvv(cvv.value)){
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return true;
+    }
+}
+
+
+form.addEventListener('submit', (e) => {
+    if(isValidName(customerName.value) && isValidEmail(email.value) && isValidPayment()){
+        return true;
+    } else{
+        e.preventDefault();
+        showEror(isValidName(customerName.value), customerName.parentElement);
+        showEror(isValidEmail(email.value), email.parentElement);
+        showEror(isValidRegister(total), activities);
+    }
+    if(payment.value === 'credit-card'){
+        showEror(isValidccNum(ccNum.value), ccNum.parentElement);
+        showEror(isValidZip(zip.value), zip.parentElement);
+        showEror(isValidCvv(cvv.value), cvv.parentElement);
+    }
+});
+
+
+
+
+// Validation in real time 
 const showOrHideTip = function(show, element){
     if(show){
+        element.classList.add('not-valid');
+		element.classList.remove('valid')
         element.style.display = 'inherit';
     } else {
         element.style.display = 'none';
@@ -145,8 +187,38 @@ function createListener(validator) {
     };
   }
 
+function showEror(validator, element){
+    if(!validator){
+        element.classList.add('not-valid');
+		element.classList.remove('valid')
+		element.lastElementChild.style.display = 'inherit';
+    } else{
+        element.classList.remove('not-valid');
+		element.classList.add('valid');
+		element.lastElementChild.style.display = 'none';
+    }
+}
+
 
 email.addEventListener("input", createListener(isValidEmail));
 ccNum.addEventListener("input", createListener(isValidccNum));
 zip.addEventListener("input", createListener(isValidZip));
 cvv.addEventListener("input", createListener(isValidCvv));
+
+
+/*
+Accessibility
+*/
+
+const checkBoxs = document.querySelectorAll("input[type='checkbox']");
+
+for(let i = 0; i < checkBoxs.length; i++){
+    checkBoxs[i].addEventListener('focus', (e) => {
+        checkBoxs[i].parentElement.classList.add('focus');
+        }
+    );
+    checkBoxs[i].addEventListener('blur', (e) => {
+        checkBoxs[i].parentElement.classList.remove('focus');
+        }
+    );
+}
